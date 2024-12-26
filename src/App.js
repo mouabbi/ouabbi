e21e33cd5e609cb09caf187da68c4ddcfabc151a
OUABBI Mohamed <Medousouabbi@gmail.com>
2024-12-26 22:34:39 +0100

    Updating Profile Image and Fixing Some style problems .

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import React from "react";
-import './index.css'
+import './index.css';
 import LandingPage from "./pages/LandingPage";
 
 function App() {
-
-
   return (
-    <BrowserRouter >
+    <BrowserRouter basename="/ouabbi">
       <Routes>
-        <Route path="/" element={<LandingPage/>} />
+        <Route path="/" element={<LandingPage />} />
       </Routes>
     </BrowserRouter>
   );
